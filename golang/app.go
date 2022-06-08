a1171713195e0ba7ae0b6c105e330d84cb17724d
logica <84759675+logica0419@users.noreply.github.com>
2022-06-09 02:02:07 +0900

    fixed syntax

--- golang/app.go
+++ golang/app.go
@@ -504,7 +504,7 @@ func htmlHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	w.Header().Add("Cache-Control", fmt.Sprintf("max-age=24h, public"))
+	w.Header().Add("Cache-Control", fmt.Sprintf("max-age=86400, public"))
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
 	io.Copy(w, file)
